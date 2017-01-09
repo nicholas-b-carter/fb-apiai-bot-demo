54cbc37fcbf5f00569ec2baa543afe4656fb0077
ncarter <ncarter@madmobile.com>
2017-01-09 16:53:15 -0500

    deploying

--- webhook.js
+++ webhook.js
@@ -16,29 +16,60 @@ const server = app.listen(process.env.PORT || 5000, () => {
 
 const apiaiHook = apiai(APIAI_TOKEN);
 
-const createOrder = ({ params, res }) => {
-  console.log(params);
-const orderRequest = request({
-    url: 'http://hamilton-api.herokuapp.com/orders',
-    method: 'POST',
-    json: params
-  }, (err, response, body) => {
-    if (!err && response.statusCode == 200) {
-      let json = JSON.parse(body);
-      console.log(json);
-      return res.json(body);
-    } else {
-      return res.status(400).json({
-        status: {
-          code: 400,
-          errorType: 'failed'
-        }
-      });
-    }
-  });
+const searchItem = item => request.get(`http://hamilton-api.herokuapp.com/search/${item}`);
 
-  console.log(orderRequest);
+const buildResponse = (returnSpeech = null, returnDisplayText = null, data = null, contextOut = [], source = null) => {
+  const speech = !returnSpeech && returnDisplayText ? returnDisplayText : returnSpeech;
+  const displayText = !returnDisplayText && returnSpeech ? returnSpeech : returnDisplayText;
+  return {
+    speech,
+    displayText,
+    data,
+    contextOut,
+    source
+  };
+}
+
+
+const findItems = (items) => {
+  const promises = [];
+  items.map(item => promises.push({ item, promise: searchItem(item) }));
+  return Promise.all(promises);
 };
+
+const createOrder = ({ params, res }) => {
+  const { delivery, item: items, quantity, quantityUom } = params;
+  const foundItems = findItems(item);
+
+  if (foundItems.length > 1) {
+    // which one did you want to choose we founds more than 1
+    res.json(buildResponse(`Which foundItems[0] would you like?`, null, foundItems, [], 'endeca'));
+  } else {
+    res.json(buildResponse(`Cool has been created!`, null, foundItems, [], 'endeca'));
+
+    // const orderRequest = request({
+    //     url: 'http://hamilton-api.herokuapp.com/orders',
+    //     method: 'POST',
+    //     json: json
+    //   }, (err, response, body) => {
+    //     if (!err && response.statusCode == 200) {
+    //       let json = JSON.parse(body);
+    //       console.log(json);
+    //       return res.json(body);
+    //     } else {
+    //       return res.status(400).json({errorType: 'failed'});
+    //     }
+    //   });
+
+      console.log(orderRequest);
+
+  }
+};
+
+
+
+
+
 
 /* Handling all messenges */
 app.post('/webhook', (req, res) => {
@@ -52,14 +83,4 @@ app.post('/webhook', (req, res) => {
       console.log('defaulted', action, parameters);
     }
   }
-
-  // apiai.on('response', (response) => {
-  //   let aiText = response.result.fulfillment.speech;
-  // });
-  //
-  // apiai.on('error', (error) => {
-  //   console.log(error);
-  // });
-  //
-  // apiai.end();
 });
