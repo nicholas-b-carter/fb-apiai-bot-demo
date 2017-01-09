4ed5d420d36227629a0724b6b2d5540c59a25708
ncarter <ncarter@madmobile.com>
2017-01-09 14:55:20 -0500

    here

--- webhook.js
+++ webhook.js
@@ -4,7 +4,7 @@ const APIAI_TOKEN = 'b20f48f876104dcfa1c542b4a5bcd2a3';
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const apiai = require('apiai');
+const api = require('apiai');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,7 +14,7 @@ const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
-const apiai = apiai(APIAI_TOKEN);
+const apiai = api(APIAI_TOKEN);
 
 const createOrder = ({ params, res }) => {
   console.log(params);
