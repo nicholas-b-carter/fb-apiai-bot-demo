93709cc17c8837d97cf96de73f368c040cd4960f
ncarter <ncarter@madmobile.com>
2017-01-09 14:53:31 -0500

    depoy

--- webhook.js
+++ webhook.js
@@ -14,7 +14,7 @@ const server = app.listen(process.env.PORT || 5000, () => {
   console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
-const apiaiApp = apiai(APIAI_TOKEN);
+const apiai = apiai(APIAI_TOKEN);
 
 const createOrder = ({ params, res }) => {
   console.log(params);
