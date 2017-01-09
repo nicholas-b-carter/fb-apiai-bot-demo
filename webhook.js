1d575e419f9c088632389b52a4da07fa1f57ae60
ncarter <ncarter@madmobile.com>
2017-01-09 13:39:17 -0500

    more

--- webhook.js
+++ webhook.js
@@ -18,42 +18,32 @@ const apiaiApp = apiai(APIAI_TOKEN);
 
 /* Handling all messenges */
 app.post('/webhook', (req, res) => {
-  console.log(req.body);
-  if (req.body.object === 'page') {
-    req.body.entry.forEach((entry) => {
-      entry.messaging.forEach((event) => {
-        if (event.message && event.message.text) {
-          receivedMessage(event);
-        }
-      });
-    });
-    res.status(200).end();
+  const { body: { result: { action, parameters } } } = req;
+  console.log(action);
+  switch action {
+    case 'create.order': {
+      console.log(action);
+      console.log(parameters);
+    }
   }
-});
-
-/* GET query from API.ai */
-
-function receivedMessage(event) {
-  let sender = event.sender.id;
-  let text = event.message.text;
-
+  // if (req.body.object === 'page') {
+  //   req.body.entry.forEach((entry) => {
+  //     entry.messaging.forEach((event) => {
+  //       if (event.message && event.message.text) {
+  //         receivedMessage(event);
+  //       }
+  //     });
+  //   });
+  //   res.status(200).end();
+  // }
+  //
   let apiai = apiaiApp.textRequest(text, {
-    sessionId: 'tabby_cat'
+    sessionId: 'sysco_ordering'
   });
 
   apiai.on('response', (response) => {
     let aiText = response.result.fulfillment.speech;
     console.log(aiText);
-
-    switch (aiText) {
-      case 'SHOW_BIOGRAPHY':
-        prepareSendBio(sender);
-        break;
-
-      default:
-        prepareSendAiMessage(sender, aiText);
-    }
-
   });
 
   apiai.on('error', (error) => {
@@ -61,78 +51,8 @@ function receivedMessage(event) {
   });
 
   apiai.end();
-}
+});
 
-function sendMessage(messageData) {
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: PAGE_ACCESS_TOKEN},
-    method: 'POST',
-    json: messageData
-  }, (error, response) => {
-    if (error) {
-        console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-        console.log('Error: ', response.body.error);
-    }
-  });
-}
-
-function prepareSendAiMessage(sender, aiText) {
-  let messageData = {
-    recipient: {id: sender},
-    message: {text: aiText}
-  };
-  sendMessage(messageData);
-}
-
-function prepareSendBio(sender) {
-  let messageData = {
-    recipient: {
-      id: sender
-    },
-    message: {
-      attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'generic',
-          elements: [{
-            title: 'Twitter',
-            subtitle: '@girlie_mac',
-            item_url: 'https://www.twitter.com/girlie_mac',
-            image_url: 'https://raw.githubusercontent.com/girliemac/fb-apiai-bot-demo/master/public/images/tomomi-twitter.png',
-            buttons: [{
-              type: 'web_url',
-              url: 'https://www.twitter.com/girlie_mac',
-              title: 'View Twitter Bio'
-            }],
-          }, {
-            title: 'Work History',
-            subtitle: 'Tomomi\'s LinkedIn',
-            item_url: 'https://www.linkedin.com/in/tomomi',
-            image_url: 'https://raw.githubusercontent.com/girliemac/fb-apiai-bot-demo/master/public/images/tomomi-linkedin.png',
-            buttons: [{
-              type: 'web_url',
-              url: 'https://www.linkedin.com/in/tomomi',
-              title: 'View LinkedIn'
-            }]
-          }, {
-            title: 'GitHub Repo',
-            subtitle: 'girliemac',
-            item_url: 'https://github.com/girliemac',
-            image_url: 'https://raw.githubusercontent.com/girliemac/fb-apiai-bot-demo/master/public/images/tomomi-github.png',
-            buttons: [{
-              type: 'web_url',
-              url: 'https://github.com/girliemac',
-              title: 'View GitHub Repo'
-            }]
-          }]
-        }
-      }
-    }
-  };
-  sendMessage(messageData);
-}
 
 /* Webhook for API.ai to get response from the 3rd party API */
 app.post('/ai', (req, res) => {
